5a383775c9a9dc2a223ace1ed010da5338170fd2
FaridHI <1121041@senati.pe>
2023-09-04 16:03:43 -0500

    cambie el nombre de los label y de los botones

--- Sistema de Ventas/Form1.Designer.cs
+++ Sistema de Ventas/Form1.Designer.cs
@@ -34,6 +34,8 @@
             label2 = new Label();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -50,34 +52,57 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(199, 55);
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(197, 45);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
+            label1.Size = new Size(86, 25);
             label1.TabIndex = 1;
-            label1.Text = "label1";
+            label1.Text = "Usuario:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(199, 136);
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(199, 125);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(118, 25);
             label2.TabIndex = 2;
-            label2.Text = "label2";
+            label2.Text = "Contraseña:";
+            label2.Click += label2_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(199, 92);
+            textBox1.Location = new Point(199, 85);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(199, 181);
+            textBox2.Location = new Point(199, 167);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(62, 223);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 38);
+            button1.TabIndex = 5;
+            button1.Text = "&Ingresar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(197, 223);
+            button2.Name = "button2";
+            button2.Size = new Size(83, 38);
+            button2.TabIndex = 6;
+            button2.Text = "&Salir";
+            button2.UseVisualStyleBackColor = true;
             // 
             // login
             // 
@@ -85,6 +110,8 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             ClientSize = new Size(369, 273);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(label2);
@@ -107,5 +134,7 @@
         private Label label2;
         private TextBox textBox1;
         private TextBox textBox2;
+        private Button button1;
+        private Button button2;
     }
 }
