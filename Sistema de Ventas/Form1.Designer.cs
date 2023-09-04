35675e1bce141573ecac70ece780755d44286f13
FaridHI <1121041@senati.pe>
2023-09-04 15:56:53 -0500

    agregue logo de tienda

--- Sistema de Ventas/Form1.Designer.cs
+++ Sistema de Ventas/Form1.Designer.cs
@@ -29,7 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources._7683ca0955f7d708cf0a760ce98c55f1;
+            pictureBox1.Location = new Point(25, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(161, 159);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // login
             // 
@@ -37,13 +49,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
             ClientSize = new Size(369, 273);
+            Controls.Add(pictureBox1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Tienda";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
     }
 }
